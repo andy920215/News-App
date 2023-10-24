f2ab523edcc0ccd7b439992526b45be92d2018be
Student <Student@205-46>
2023-10-24 16:02:11 +0800

    加入專案檔案。

---
+++ News App/Pages/NewsHomePage.xaml.cs
@@ -0,0 +1,14 @@
+namespace News_App.Pages;
+
+public partial class NewsHomePage : ContentPage
+{
+	public NewsHomePage()
+	{
+		InitializeComponent();
+	}
+
+    private void CvCategorles_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+
+    }
+}
